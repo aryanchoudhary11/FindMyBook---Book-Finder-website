a8ff03b5a3607b86579776cc7844fb31d5b12935
Aryan <119430003+aryanchoudhary11@users.noreply.github.com>
2025-01-21 11:03:06 +0530

    popular books details content added

--- popular-books.js
+++ popular-books.js
@@ -10,12 +10,17 @@ if(book){
             <img src="${book.cover}">
             <button>Read Now</button>
             <button>Buy Now</button>
+            <p id="price">${book.price}</p>
         </div>
         <div class="popular-details-container">
             <h1>${book.title}</h1>
             <p id="author-name">${book.author}</p>
+            <p id="genre"><b>Genre: </b>${book.genre}</p>
             <p id="intro">${book.intro}</p>
             <span>${book.pages} pages</span>
+            <h3>Extra details</h3>
+            <p id="original_t"><b>Original title: </b>${book.originalTitle}</p>
             <span>First published in ${book.published}</span>
+            <p id="awards"><b>Literary awards: </b>${book.awards}</p>
         </div>`
 }
